a51fd6abe0e1275dae0ae05ada0d758580bcea01
spences10 <spences10apps@gmail.com>
2017-08-09 19:47:15 +0100

    add networkInterface

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj65axyo405r00187t3mmp5ly'
+})
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
