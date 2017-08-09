781fdf8c857bc980ba79044f844e71a4058089a2
spences10 <spences10apps@gmail.com>
2017-08-09 22:21:15 +0100

    Styles!

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,17 @@ import Header from './Header'
 
 class App extends Component {
   render() {
-    return <CreateLink />
+    return (
+      <div className='center w85'>
+        <Header />
+        <div className='ph3 pv1 background-gray'>
+          <Switch>
+            <Route exact path='/' component={LinkList} />
+            <Route exact path='/create' component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
+    )
   }
 }
 
