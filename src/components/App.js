cc286f51c46156ad651fdb025ddc1b02ddde708e
spences10 <spences10apps@gmail.com>
2017-08-10 14:13:08 +0100

    Filtering: Searching the List of Links

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Login from './Login'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
+import Search from './Search'
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Header />
         <div className='ph3 pv1 background-gray'>
           <Switch>
+            <Route exact path='/search' component={Search} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/' component={LinkList} />
             <Route exact path='/create' component={CreateLink} />
